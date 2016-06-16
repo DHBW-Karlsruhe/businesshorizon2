36431ee47ce203aad50ee84a6adfd3ffca512bb0
seazzle <seazzle@seazzle-LENOVO>
2016-06-16 10:29:54 +0200

    UserProjects model class used as root element in PersistanceService

---
+++ businesshorizon2/src/main/java/dhbw/ka/mwi/businesshorizon2/models/UserProjects.java
@@ -0,0 +1,31 @@
+package dhbw.ka.mwi.businesshorizon2.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.simpleframework.xml.ElementList;
+import org.simpleframework.xml.Root;
+/**
+ * @author Peter Kaisinger
+ * Root Klasse für das Serialisieren der Projekte nach XML
+ */
+@Root
+public class UserProjects {
+	
+	@ElementList
+	private List<Project> projects = new ArrayList<Project>();
+	
+	
+	public List<Project> getProjects() {
+		return projects;
+	}
+	
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
+	
+	public void addProject(Project project){
+		projects.add(project);
+	}
+	
+}
