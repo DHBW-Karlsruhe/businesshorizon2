4d25bba9d49e337090dd875ea972ca36c189f418
AnhNgchen <thuy-anh.nguyen@outlook.de>
2016-07-04 10:45:07 +0200

    change Test

--- businesshorizon2/src/test/java/dhbw/ka/mwi/businesshorizon2/methods/timeseries/TestAutokovarianz.java
+++ businesshorizon2/src/test/java/dhbw/ka/mwi/businesshorizon2/methods/timeseries/TestAutokovarianz.java
@@ -2,7 +2,6 @@ package dhbw.ka.mwi.businesshorizon2.methods.timeseries;
 
 import static org.junit.Assert.*;
 
-import org.apache.jasper.tagplugins.jstl.ForEach;
 import org.junit.Test;
 
 public class TestAutokovarianz {
@@ -10,15 +9,11 @@ public class TestAutokovarianz {
 	@Test
 	public void testBerechneAutokovarianz() throws Exception {
 		
-		int n = 4;
-		double [] testZeitreihe = new double[n];
-		testZeitreihe[0] = 0;
-		testZeitreihe[1] = 1;
-		testZeitreihe[2] = 2;
-		testZeitreihe[3] = 3;
+		//int n = 4;
+		double [] testZeitreihe = {0,1,2,3};
 		
 		for (int i =0; i<testZeitreihe.length; i++) {
-			System.out.println("Zeitreihe["+i+"]: " + testZeitreihe);
+			System.out.println("Zeitreihe["+i+"]: " + testZeitreihe[i]);
 		}
 				
 		int i_TestLag = 1;
