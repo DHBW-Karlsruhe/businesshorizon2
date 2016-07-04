758b8f202a6964906a4d36f74db11b94022da50e
AnhNgchen <thuy-anh.nguyen@outlook.de>
2016-07-04 12:29:54 +0200

    change test

--- businesshorizon2/src/test/java/dhbw/ka/mwi/businesshorizon2/methods/timeseries/TestAutokovarianz.java
+++ businesshorizon2/src/test/java/dhbw/ka/mwi/businesshorizon2/methods/timeseries/TestAutokovarianz.java
@@ -10,21 +10,27 @@ public class TestAutokovarianz {
 	public void testBerechneAutokovarianz() throws Exception {
 		
 		//int n = 4;
-		double [] testZeitreihe = {0,1,2,3};
+		AnalysisTimeseries at = new AnalysisTimeseries();
+		double [] testZeitreihe = {8,12,10,14,12,16};
+		testZeitreihe = at.reduceTide(testZeitreihe);
 		
 		for (int i =0; i<testZeitreihe.length; i++) {
 			System.out.println("Zeitreihe["+i+"]: " + testZeitreihe[i]);
 		}
 				
-		int i_TestLag = 1;
+		int i_TestLag = 0;
 		System.out.println("Lag: " + i_TestLag);
 		
 		//double db_TestAutocovariance = 0;
 		//System.out.println("Autovarianz: " + db_TestAutocovariance);
 		
-		AnalysisTimeseries atTest = new AnalysisTimeseries();		
-		System.out.println("Autokovarianz: " + atTest.calculateAutocovariance(testZeitreihe, i_TestLag));
-		
+		AnalysisTimeseries atTest = new AnalysisTimeseries();	
+		for(int i = 0; i<testZeitreihe.length;i++){
+			i_TestLag = i;
+			System.out.println("Autokovarianz mit Lag "+ i_TestLag+": " + atTest.calculateAutocovariance(testZeitreihe, i_TestLag));
+			System.out.println("Autokorrelation mit Lag "+ i_TestLag+": " + atTest.calculateAutocorrelation(testZeitreihe, i_TestLag));
+		}
+				
 	}
 
 }
