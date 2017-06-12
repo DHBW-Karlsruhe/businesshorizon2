1d36ef355774e1ef335f1a7cd9f237865fcf909e
Dennis <dennis.pruefer@gmail.com>
2017-06-12 15:00:55 +0200

    terniary durch if ersetzt

--- businesshorizon2-cf/src/main/java/dhbw/ka/mwi/businesshorizon2/cfsimple/APV.java
+++ businesshorizon2-cf/src/main/java/dhbw/ka/mwi/businesshorizon2/cfsimple/APV.java
@@ -3,11 +3,19 @@ package dhbw.ka.mwi.businesshorizon2.cfsimple;
 public class APV implements CFAlgorithm {
 
     private static double getUWFiktiv(final CFParameter parameter, final int periode) {
-        return periode < parameter.numPerioden() - 2 ? getUWFiktiv(parameter, periode + 1) + parameter.getFCF()[periode + 1] / Math.pow(1 + parameter.getEKKosten(), periode + 1) : parameter.getFCF()[periode + 1] / (parameter.getEKKosten() * Math.pow(1 + parameter.getEKKosten(), periode));
+        if (periode < parameter.numPerioden() - 2) {
+            return getUWFiktiv(parameter, periode + 1) + parameter.getFCF()[periode + 1] / Math.pow(1 + parameter.getEKKosten(), periode + 1);
+        } else {
+            return parameter.getFCF()[periode + 1] / (parameter.getEKKosten() * Math.pow(1 + parameter.getEKKosten(), periode));
+        }
     }
 
     private static double getTaxShield(final CFParameter parameter, final int periode) {
-        return periode < parameter.numPerioden() - 2 ? getTaxShield(parameter, periode + 1) + parameter.getuSteusatz() * parameter.getFKKosten() * parameter.getFK()[periode] / Math.pow(parameter.getFKKosten() + 1, periode + 1) : parameter.getuSteusatz() * parameter.getFK()[periode] / Math.pow(parameter.getFKKosten() + 1, periode);
+        if (periode < parameter.numPerioden() - 2) {
+            return getTaxShield(parameter, periode + 1) + parameter.getuSteusatz() * parameter.getFKKosten() * parameter.getFK()[periode] / Math.pow(parameter.getFKKosten() + 1, periode + 1);
+        } else {
+            return parameter.getuSteusatz() * parameter.getFK()[periode] / Math.pow(parameter.getFKKosten() + 1, periode);
+        }
     }
 
     private static double getGesamtkapital(final CFParameter parameter, final int periode) {
