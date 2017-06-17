85cdef9d6323e7848dca2ab48ea89ebf4cb0b569
Dennis <dennis.pruefer@gmail.com>
2017-06-17 20:57:06 +0200

    Fehler bei der Berechnung von EK Kosten Verschuldet behoben

--- businesshorizon2-cf/src/main/java/dhbw/ka/mwi/businesshorizon2/cfsimple/EKKostVerschCalculator.java
+++ businesshorizon2-cf/src/main/java/dhbw/ka/mwi/businesshorizon2/cfsimple/EKKostVerschCalculator.java
@@ -7,11 +7,10 @@ final class EKKostVerschCalculator {
     static double getEKKostenVersch(final CFParameter parameter, final CFIntermediateResult intermediate, final int periode){
         double summe = 0;
 
-        for (int i = 0; i < parameter.numPerioden() - periode - 1; i++) {
-            summe+= parameter.getuSteusatz() * parameter.getFKKosten() * parameter.getFK()[i] / Math.pow(1 + parameter.getFKKosten(), i + 1);
+        for (int i = periode; i < parameter.numPerioden() - 1; i++) {
+            summe+= parameter.getuSteusatz() * parameter.getFKKosten() * parameter.getFK()[i - 1] / Math.pow(1 + parameter.getFKKosten(), periode - (i - 1));
         }
-
-        final double sfkt = parameter.getuSteusatz() * parameter.getFK()[parameter.numPerioden() -1] / Math.pow(1 + parameter.getFKKosten(),parameter.numPerioden() - periode - 1);
+        final double sfkt = parameter.getuSteusatz() * parameter.getFK()[parameter.numPerioden() - 2] / Math.pow(1 + parameter.getFKKosten(),parameter.numPerioden() - periode - 1);
         final double fkt1Summe = parameter.getFK()[periode - 1] - summe - sfkt;
         final double subtraktion = fkt1Summe / intermediate.getuWert()[periode - 1];
         return parameter.getEKKosten() + (parameter.getEKKosten() - parameter.getFKKosten()) * subtraktion;
