fb60c29fae7a56d87d69e6214fff51cba631e590
Dennis <dennis.pruefer@gmail.com>
2017-06-18 13:09:05 +0200

    Inteface sollte public sein

--- businesshorizon2-cf/src/main/java/dhbw/ka/mwi/businesshorizon2/cfsimple/EKKostVerschCalculator.java
+++ businesshorizon2-cf/src/main/java/dhbw/ka/mwi/businesshorizon2/cfsimple/EKKostVerschCalculator.java
@@ -1,7 +1,7 @@
 package dhbw.ka.mwi.businesshorizon2.cfsimple;
 
 @FunctionalInterface
-interface EKKostVerschCalculator {
+public interface EKKostVerschCalculator {
 
     double calculateEKKostenVersch(final CFParameter parameter, final CFIntermediateResult intermediate, final int periode);
 
