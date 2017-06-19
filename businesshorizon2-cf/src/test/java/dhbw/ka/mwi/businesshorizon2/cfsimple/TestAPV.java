adfa29567f864a7aa5ff85c29fe2b2e871771921
Dennis <dennis.pruefer@gmail.com>
2017-06-19 12:56:47 +0200

    Präzision der Testfälle ist nun abhängig von der Größe der Variablen

--- businesshorizon2-cf/src/test/java/dhbw/ka/mwi/businesshorizon2/cfsimple/TestAPV.java
+++ businesshorizon2-cf/src/test/java/dhbw/ka/mwi/businesshorizon2/cfsimple/TestAPV.java
@@ -8,18 +8,18 @@ public class TestAPV {
     @Test
     public void testAPVYannik() throws Exception {
         final CFParameter parameter = new CFParameter(new double[]{3960000000d,4158000000d,4365900000d,4584195000d,4813404750d,5054074987.5d,5054074987.5d},new double[]{162542000000d,177170780000d,193116150200d,210496603718d,229441298053d,250091014877d,250091014877d},0.1,0.26325,0.01);
-        assertEquals(-48779385984d,new APV().calculateUWert(parameter),1);
+        assertEquals(-48779385984d,new APV().calculateUWert(parameter),48779385984d / 10000);
     }
 
     @Test
     public void testAPVBallwieser() throws Exception {
         final CFParameter parameter = new CFParameter(new double[]{0,138.61,202.31,174.41,202.51},new double[]{1260,1320,1330,1400,1400},0.09969137,0.26325,0.08);
-        assertEquals(1055.24,new APV().calculateUWert(parameter),0.1);
+        assertEquals(1055.24,new APV().calculateUWert(parameter),1055.24 / 10000);
     }
 
     @Test
     public void testFCFPohl() throws Exception {
         final CFParameter parameter = new CFParameter(new double[]{0,176.76,520.13,404.87,203.78},new double[]{1260,1300,1000,1400,1400},0.100582,0.3063,0.08);
-        assertEquals(1569.19,new APV().calculateUWert(parameter),0.1);
+        assertEquals(1569.19,new APV().calculateUWert(parameter),1569.19 / 10000);
     }
 }
--- businesshorizon2-cf/src/test/java/dhbw/ka/mwi/businesshorizon2/cfsimple/TestTaxShieldCalculator.java
+++ businesshorizon2-cf/src/test/java/dhbw/ka/mwi/businesshorizon2/cfsimple/TestTaxShieldCalculator.java
@@ -9,11 +9,11 @@ public class TestTaxShieldCalculator {
     @Test
     public void testTaxShield() throws Exception {
         final CFParameter parameter = new CFParameter(new double[]{0,138.61,202.31,174.41,202.51},new double[]{1260,1320,1330,1400,1400},0.09969137,0.26325,0.08);
-        assertEquals(363.21,TaxShieldCalculator.calculateTaxShield(parameter,0),0.01);
-        assertEquals(365.73,TaxShieldCalculator.calculateTaxShield(parameter,1),0.01);
-        assertEquals(367.19,TaxShieldCalculator.calculateTaxShield(parameter,2),0.01);
-        assertEquals(368.55,TaxShieldCalculator.calculateTaxShield(parameter,3),0.01);
-        assertEquals(368.55,TaxShieldCalculator.calculateTaxShield(parameter,4),0.01);
+        assertEquals(363.21,TaxShieldCalculator.calculateTaxShield(parameter,0),363.21 / 10000);
+        assertEquals(365.73,TaxShieldCalculator.calculateTaxShield(parameter,1),365.73 / 10000);
+        assertEquals(367.19,TaxShieldCalculator.calculateTaxShield(parameter,2),367.19 / 10000);
+        assertEquals(368.55,TaxShieldCalculator.calculateTaxShield(parameter,3),368.55 / 10000);
+        assertEquals(368.55,TaxShieldCalculator.calculateTaxShield(parameter,4),368.55 / 10000);
     }
 
 }
