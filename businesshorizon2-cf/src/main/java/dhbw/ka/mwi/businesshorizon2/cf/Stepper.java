64791d7a000dafeb4a862571003b7989b54fc8f4
Dennis <dennis.pruefer@gmail.com>
2017-06-20 12:03:05 +0200

    Maximale Anzahl an Loops begrenzt

--- businesshorizon2-cf/src/main/java/dhbw/ka/mwi/businesshorizon2/cf/Stepper.java
+++ businesshorizon2-cf/src/main/java/dhbw/ka/mwi/businesshorizon2/cf/Stepper.java
@@ -4,17 +4,22 @@ import java.util.function.Function;
 
 final class Stepper {
 
+    private static final int MAX_LOOPS = 500;
+
     private Stepper() {
     }
 
     static CFIntermediateResult performStepping(final CFIntermediateResult startValues, final Function<CFIntermediateResult, CFIntermediateResult> stepFunction){
         CFIntermediateResult intermediate = startValues;
-        while (true){
+        int loops = 0;
+        while (loops <= MAX_LOOPS){
             final CFIntermediateResult next = stepFunction.apply(intermediate);
             if(Math.abs(next.getuWert()[0] - intermediate.getuWert()[0]) < CFConfig.getPrecision()){
                 return next;
             }
             intermediate = next;
+            loops++;
         }
+        return intermediate;
     }
 }
