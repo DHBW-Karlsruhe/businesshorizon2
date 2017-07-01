6099b4bfb2bb7b25bf4211acc82607d10e75d815
Dennis <dennis.pruefer@gmail.com>
2017-07-01 20:41:55 +0200

    War nicht nötig

--- businesshorizon2-ar/src/main/java/dhbw/ka/mwi/businesshorizon2/ar/predictor/RandomWalk.java
+++ businesshorizon2-ar/src/main/java/dhbw/ka/mwi/businesshorizon2/ar/predictor/RandomWalk.java
@@ -1,17 +1,23 @@
 package dhbw.ka.mwi.businesshorizon2.ar.predictor;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 public class RandomWalk {
 
+    private final Random random;
     private final double stdDev;
 
     public RandomWalk(final double stdDev) {
+        this(new Random(), stdDev);
+    }
+
+    RandomWalk(final Random random, final double stdDev) {
+        this.random = random;
         this.stdDev = stdDev;
     }
 
     double getRandomWalk() {
-        return stdDev * (ThreadLocalRandom.current().nextBoolean() ? 1 : -1);
+        return stdDev * (random.nextBoolean() ? 1 : -1);
     }
 
 }
--- businesshorizon2-ar/src/test/java/dhbw/ka/mwi/businesshorizon2/ar/predictor/TestARPredictorRunner.java
+++ businesshorizon2-ar/src/test/java/dhbw/ka/mwi/businesshorizon2/ar/predictor/TestARPredictorRunner.java
@@ -1,6 +1,9 @@
 package dhbw.ka.mwi.businesshorizon2.ar.predictor;
 
+import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static org.junit.Assert.assertArrayEquals;
 
@@ -16,4 +19,14 @@ public class TestARPredictorRunner {
         assertArrayEquals(new double[]{73383.35, 77485.07}, predictions, 0.01);
     }
 
+    @Test
+    public void testRandomWalk() {
+        final double[] timeSeries = {77327d, 76651d, 75978d, 73515d, 78296d, 75882d, 71920d, 75636d,
+                79644d};
+        final double[] coeffs = {-0.21244, -0.79116};
+
+        final double stdDev = new StandardDeviation(false).evaluate(timeSeries);
+        final double[] predictions = new ARPredictorRunner(new RandomWalkPredictor(new RandomWalk(new Random(1), stdDev))).runPredictions(timeSeries, coeffs, 2);
+        assertArrayEquals(new double[]{75584.95, 73541.64}, predictions, 0.01);
+    }
 }
