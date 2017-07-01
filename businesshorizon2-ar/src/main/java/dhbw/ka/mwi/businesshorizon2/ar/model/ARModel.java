9907a99f870129d9f6783c436fb15cefc6c0378f
DanielWarkentin <daniel.warkentin95@gmail.com>
2017-07-01 16:46:55 +0200

    Update ARModel.java
    Implementierung des Güte Kriteriums für AR-Modelle

--- businesshorizon2-ar/src/main/java/dhbw/ka/mwi/businesshorizon2/ar/model/ARModel.java
+++ businesshorizon2-ar/src/main/java/dhbw/ka/mwi/businesshorizon2/ar/model/ARModel.java
@@ -1,5 +1,7 @@
 package dhbw.ka.mwi.businesshorizon2.ar.model;
 
+import org.apache.commons.math3.stat.descriptive.moment.Mean;
+
 import dhbw.ka.mwi.businesshorizon2.ar.predictor.ARPredictor;
 import dhbw.ka.mwi.businesshorizon2.ar.predictor.ARPredictorRunner;
 
@@ -24,8 +26,27 @@ public class ARModel {
     public double[] predict(final int numPeriods) {
         return new ARPredictorRunner(new ARPredictor()).runPredictions(timeSeries, coefficients, numPeriods);
     }
+    
+    /* Berechnet die Güte des AR-Modells
+     * Also wie gut das AR-Modell auf die Zeitreihe passt
+     */
+    public double calculateQuality() {
+    	double result = 0;
+    	final double avg = new Mean().evaluate(timeSeries);
+    	for(int i = coefficients.length+1; i < timeSeries.length;i++){
+    		double prognose = 0;
+    		for (int j = 0; j < coefficients.length; j++) {
+    			prognose += coefficients[j] * (timeSeries[j]-avg);
+    		}
+    		double error = timeSeries[i]-avg-prognose;
+    		
+    		result += error*error;
+    	}
+    	return Math.sqrt(result/(timeSeries.length-coefficients.length))/avg;
+    }
 
     public double[] getCoefficients() {
         return coefficients;
     }
 }
+
