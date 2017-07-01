6c4d998858c2474788f6d664b751cba558c5bf48
Tobias Gießler <to.gie94@web.de>
2017-07-01 16:47:15 +0200

    Korrektur Präzison APV und FTE. Implementierung von assertRelative.

--- businesshorizon2-cf/src/test/java/dhbw/ka/mwi/businesshorizon2/cf/TestAPV.java
+++ businesshorizon2-cf/src/test/java/dhbw/ka/mwi/businesshorizon2/cf/TestAPV.java
@@ -3,36 +3,38 @@ package dhbw.ka.mwi.businesshorizon2.cf;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import static dhbw.ka.mwi.businesshorizon2.cf.AssertRelative.assertRelative;
+
 public class TestAPV {
 
     @Test
     public void testAPVYannik() throws Exception {
         final CFParameter parameter = new CFParameter(new double[]{3960000000d,4158000000d,4365900000d,4584195000d,4813404750d,5054074987.5d,5054074987.5d},new double[]{162542000000d,177170780000d,193116150200d,210496603718d,229441298053d,250091014877d,250091014877d},0.1,0.26325,0.01);
         final APVResult result = new APV().calculateUWert(parameter);
-        assertEquals(-48779385984d,result.getuWert(),48779385984d / 10000);
-        assertEquals(65122626224d,result.getTaxShield(),65122626224d / 10000);
-        assertEquals(48639987791d,result.getUwFiktiv(),48639987791d / 10000);
-        assertEquals(113762614016d,result.getGk(),113762614016d / 10000);
+        assertRelative(-48779385984d,result.getuWert());
+        assertRelative(65122626224d,result.getTaxShield());
+        assertRelative(48639987791d,result.getUwFiktiv());
+        assertRelative(113762614016d,result.getGk());
     }
 
     @Test
     public void testAPVBallwieser() throws Exception {
         final CFParameter parameter = new CFParameter(new double[]{0,138.61,202.31,174.41,202.51},new double[]{1260,1320,1330,1400,1400},0.09969137,0.26325,0.08);
         final APVResult result = new APV().calculateUWert(parameter);
-        assertEquals(1055.24,result.getuWert(),1055.24 / 10000);
-        assertEquals(363.21,result.getTaxShield(),363.21 / 10000);
-        assertEquals(1952.03,result.getUwFiktiv(),1952.03 / 10000);
-        assertEquals(2315.24,result.getGk(),2315.24 / 10000);
+        assertRelative(1055.24,result.getuWert());
+        assertRelative(363.21,result.getTaxShield());
+        assertRelative(1952.03,result.getUwFiktiv());
+        assertRelative(2315.24,result.getGk());
     }
 
     @Test
     public void testFCFPohl() throws Exception {
         final CFParameter parameter = new CFParameter(new double[]{0,176.76,520.13,404.87,203.78},new double[]{1260,1300,1000,1400,1400},0.100582,0.30625,0.08);
         final APVResult result = new APV().calculateUWert(parameter);
-        assertEquals(1569.19,result.getuWert(),1569.19 / 10000);
-        assertEquals(415.69,result.getTaxShield(),415.69 / 10000);
-        assertEquals(2413.50,result.getUwFiktiv(),2413.50 / 10000);
-        assertEquals(2829.19,result.getGk(),2829.19 / 10000);
+        assertRelative(1569.19,result.getuWert());
+        assertRelative(415.69,result.getTaxShield());
+        assertRelative(2413.50,result.getUwFiktiv());
+        assertRelative(2829.19,result.getGk());
     }
     
     //Randbereiche und Sonderfälle, Excel-Pohl *********************************************************************************************
@@ -43,10 +45,10 @@ public class TestAPV {
     public void nullEuroWerte() throws Exception {
     	final CFParameter parameter = new CFParameter(new double[]{0,0,0,0,0},new double[]{0,0,0,0,0},0.100582,0.30625,0.08);
         final APVResult result = new APV().calculateUWert(parameter);
-        assertEquals(0.0, result.getuWert(), 0);
-        assertEquals(0.0, result.getTaxShield(), 0);
-        assertEquals(0.0, result.getUwFiktiv(), 0);
-        assertEquals(0.0, result.getGk(), 0);
+        assertRelative(0.0, result.getuWert());
+        assertRelative(0.0, result.getTaxShield());
+        assertRelative(0.0, result.getUwFiktiv());
+        assertRelative(0.0, result.getGk());
     }
     
     //1€-Test
@@ -55,10 +57,10 @@ public class TestAPV {
     public void testEinEuroWerte() throws Exception {
     	final CFParameter parameter = new CFParameter(new double[]{0,1.0,1.0,1.0,1.0},new double[]{1.0,1.0,1.0,1.0,1.0},0.100582,0.30625,0.08);
         final APVResult result = new APV().calculateUWert(parameter);
-        assertEquals(9.25, result.getuWert(), 0.01);
-        assertEquals(0.31, result.getTaxShield(), 0.01); //Ergebis 0.30629
-        assertEquals(9.94, result.getUwFiktiv(), 0.011);
-        assertEquals(10.25, result.getGk(), 0.01); //Ergebnis 10.2465
+        assertRelative(9.248386764, result.getuWert());
+        assertRelative(0.30625, result.getTaxShield());
+        assertRelative(9.94213676403333, result.getUwFiktiv());
+        assertRelative(10.2483867640333, result.getGk());
     }
     
     //Milliardenwerte-Test
@@ -66,11 +68,10 @@ public class TestAPV {
     public void milliardenEuroWerte() throws Exception {
     	final CFParameter parameter = new CFParameter(new double[]{0, 1450000000.0, 1260000000.0, 3340000000.0, 1680000000.0},new double[]{1360000000.0, 1600000000.0, 2200000000.0, 1500000000.0, 1500000000.0},0.100582,0.30625,0.08);
         final APVResult result = new APV().calculateUWert(parameter);
-        assertEquals(16479876705.852, result.getuWert(), 16479876705.852 / 100);
-        //Fehler
-//        assertEquals(447602664.73, result.getTaxShield(), 447602664.73 / 100);
-        assertEquals(17392274041.11, result.getUwFiktiv(), 17392274041.11 / 100);
-        assertEquals(17839876705.85, result.getGk(), 17839876705.85 / 100);
+        assertRelative(16504187818.2333000000, result.getuWert());
+        assertRelative(471913777.1173090000, result.getTaxShield());
+        assertRelative(17392274041.1160000000, result.getUwFiktiv());
+        assertRelative(17864187818.233300000000, result.getGk());
     }
     
     //Negative Werte-Test
@@ -78,10 +79,10 @@ public class TestAPV {
     public void negativeEuroWerte() throws Exception {
     	final CFParameter parameter = new CFParameter(new double[]{0, 176.76, -520.13, 404.87, -203.78},new double[]{-1260, -1300, 1000, -1400, -1400},0.100582,0.30625,0.08);
         final APVResult result = new APV().calculateUWert(parameter);
-        assertEquals(-601.67,result.getuWert(), 601.67 / 10000);
-        assertEquals(-376.80,result.getTaxShield(), 376.80 / 10000);
-        assertEquals(-1484.87,result.getUwFiktiv(), 1484.87 / 10000);
-        assertEquals(-1861.67,result.getGk(), 1861.67 / 10000);
+        assertRelative(-601.6495353431,result.getuWert());
+        assertRelative(-376.796258192349,result.getTaxShield());
+        assertRelative(-1484.85327715077,result.getUwFiktiv());
+        assertRelative(-1861.6495353431,result.getGk());
     }
     
     //Kleine, gemischte Werte-Test
@@ -89,10 +90,9 @@ public class TestAPV {
     public void kleineGemischteEuroWerte() throws Exception {
     	final CFParameter parameter = new CFParameter(new double[]{0, 1.12, 0.78, 0.56, 1.01},new double[]{0.34, 1.2, 0.8, 0.51, 0.51},0.100582,0.30625,0.08);
         final APVResult result = new APV().calculateUWert(parameter);
-        assertEquals(9.45, result.getuWert(), 9.45 / 100);
-        //Tax-Shield-Fehler -> Ergebnis 0.172465 -> Toleranz muss in den 10tel Bereich
-//        assertEquals(0.17, result.getTaxShield(), 0.17 / 100);
-        assertEquals(9.61, result.getUwFiktiv(), 9.61 / 100);
-        assertEquals(9.79, result.getGk(), 9.79 / 100);
+        assertRelative(9.4465325137, result.getuWert());
+        assertRelative(0.172464509348168, result.getTaxShield());
+        assertRelative(9.61406800431095, result.getUwFiktiv());
+        assertRelative(9.7865325137, result.getGk());
     }
 }
--- businesshorizon2-cf/src/test/java/dhbw/ka/mwi/businesshorizon2/cf/TestFTE.java
+++ businesshorizon2-cf/src/test/java/dhbw/ka/mwi/businesshorizon2/cf/TestFTE.java
@@ -3,18 +3,19 @@ package dhbw.ka.mwi.businesshorizon2.cf;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static dhbw.ka.mwi.businesshorizon2.cf.AssertRelative.assertRelative;
 
 public class TestFTE {
     @Test
     public void testFTEBallwieser() throws Exception {
         final CFParameter parameter = new CFParameter(new double[]{0,138.61,202.31,174.41,202.51},new double[]{1260,1320,1330,1400,1400},0.09969137,0.26325,0.08);
-        assertEquals(1055.24,new FTE().calculateUWert(parameter).getuWert(),1055.24 / 10000);
+        assertRelative(1055.24,new FTE().calculateUWert(parameter).getuWert());
     }
 
     @Test
     public void testFTEPohl() throws Exception {
         final CFParameter parameter = new CFParameter(new double[]{0,176.76,520.13,404.87,203.78},new double[]{1260,1300,1000,1400,1400},0.100582,0.30625,0.08);
-        assertEquals(1569.19,new FTE().calculateUWert(parameter).getuWert(),1569.19  / 10000);
+        assertRelative(1569.19,new FTE().calculateUWert(parameter).getuWert());
     }
     
     //Randbereiche und Sonderfälle, Excel-Pohl *********************************************************************************************
@@ -24,7 +25,7 @@ public class TestFTE {
     @Test
     public void nullEuroWerte() throws Exception {
     	final CFParameter parameter = new CFParameter(new double[]{0,0,0,0,0},new double[]{0,0,0,0,0},0.100582,0.30625,0.08);
-    	assertEquals(0.0, new FTE().calculateUWert(parameter).getuWert(), 0);
+    	assertRelative(0.0, new FTE().calculateUWert(parameter).getuWert());
     }
     
     //1€-Test
@@ -32,28 +33,27 @@ public class TestFTE {
     @Test
     public void testEinEuroWerte() throws Exception {
     	final CFParameter parameter = new CFParameter(new double[]{0,1.0,1.0,1.0,1.0},new double[]{1.0,1.0,1.0,1.0,1.0},0.100582,0.30625,0.08);
-    	assertEquals(9.25, new FTE().calculateUWert(parameter).getuWert(), 0.01);
+    	assertRelative(9.248386764, new FTE().calculateUWert(parameter).getuWert());
     }
     
     //Milliardenwerte-Test
     @Test
     public void milliardenEuroWerte() throws Exception {
     	final CFParameter parameter = new CFParameter(new double[]{0, 1450000000.0, 1260000000.0, 3340000000.0, 1680000000.0},new double[]{1360000000.0, 1600000000.0, 2200000000.0, 1500000000.0, 1500000000.0},0.100582,0.30625,0.08);
-        assertEquals(16479876705.852, new FTE().calculateUWert(parameter).getuWert(), 16479876705.852 / 100);
+    	assertRelative(16504187818.2333000000, new FTE().calculateUWert(parameter).getuWert());
     }
     
     //Negative Werte-Test
     @Test
     public void negativeEuroWerte() throws Exception {
     	final CFParameter parameter = new CFParameter(new double[]{0, 176.76, -520.13, 404.87, -203.78},new double[]{-1260, -1300, 1000, -1400, -1400},0.100582,0.30625,0.08);
-    	assertEquals( -601.67, new FTE().calculateUWert(parameter).getuWert(), 601.67 / 10000);
+    	assertRelative(-601.6495353431, new FTE().calculateUWert(parameter).getuWert());
     }
     
     //Kleine, gemischte Werte-Test
     @Test
     public void kleineGemischteEuroWerte() throws Exception {
     	final CFParameter parameter = new CFParameter(new double[]{0, 1.12, 0.78, 0.56, 1.01},new double[]{0.34, 1.2, 0.8, 0.51, 0.51},0.100582,0.30625,0.08);
-        final APVResult result = new APV().calculateUWert(parameter);
-        assertEquals(9.45, new FTE().calculateUWert(parameter).getuWert(), 9.45 / 100);
+        assertRelative(9.4465325137, new FTE().calculateUWert(parameter).getuWert());
     }
 }
